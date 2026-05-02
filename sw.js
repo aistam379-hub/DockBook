e7d3eb5c9883bc552dd835f008ef01c1aea821fc
aistam379-hub <aistam379@gmail.com>
2026-05-02 21:19:24 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,4 @@
-const CACHE='docbook-nurse-v2';
-const ASSETS=['./','./index.html','./manifest.json','./'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
-});
+const CACHE='docbook-nurse-v4';const ASSETS=['./','./index.html','./manifest.json','./icon-192.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(
-    caches.open(CACHE).then(cache=>
-      cache.match(e.request).then(hit=>{
-        var fresh=fetch(e.request).then(r=>{
-          if(r&&r.status===200)cache.put(e.request,r.clone());
-          return r;
-        }).catch(()=>hit);
-        return hit||fresh;
-      })
-    )
-  );
-});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.open(CACHE).then(cache=>cache.match(e.request).then(hit=>{var f=fetch(e.request).then(r=>{if(r&&r.status===200)cache.put(e.request,r.clone());return r;}).catch(()=>hit);return hit||f;})));});
